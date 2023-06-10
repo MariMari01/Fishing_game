d163723d8257dfe3e49b7ee8bcbd30f029ef6bdf
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2023-06-10 12:39:42 -0700

    Update fisher_cat_class.py

--- fisher_cat_class.py
+++ fisher_cat_class.py
@@ -1,3 +1,7 @@
+'''This file contains the class for the fishing 
+cat player character
+'''
+
 import pygame
 
 class FisherCat:
@@ -6,27 +10,47 @@ class FisherCat:
     of the fisher man cat
     '''
     def __init__(self,x,y, window_x) -> None:
+        self.fishing_bob = pygame.image.load("fishing_bob.png")
         self.image = pygame.image.load("Ship_full.png")
         self.rect = self.image.get_rect()
+        self.bob_rect = self.fishing_bob.get_rect()
         self.xpos = x
         self.ypos = y
         self.win_x = window_x
+    
+    def draw(self, screen):
+        '''
+        Draws the fisher cat object onto the screen
+        '''
+        screen.blit(self.fishing_bob, self.bob_rect)
+        screen.blit(self.image, self.rect)
+
     def move_left(self):
+        '''
+        Moves the fisher cat left
+        '''
         if self.xpos == 0:
             pass
         else:
             self.xpos -= 0.5
             self.rect.center = (self.xpos, self.ypos)
+            self.bob_rect.center = (self.xpos, self.ypos)
 
     def move_right(self):
+        '''
+        Moves the fisher cat right
+        '''
         if self.xpos == self.win_x:
             pass
         else:
             self.xpos += 0.5
             self.rect.center = (self.xpos, self.ypos)
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+            self.rect.center = (self.xpos, self.ypos)
+            self.bob_rect.center = (self.xpos, self.ypos)
 
 
-    def cast(self):
-        pass
+    def cast(cls):
+        '''
+        Casts the fishing rod 
+        '''
+        cls.bob_rect.center = (cls.xpos, cls.ypos + 400)
