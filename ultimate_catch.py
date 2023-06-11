1f5879160a14c2940821f9dd666c076b4c316c1f
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-11 02:08:46 -0700

    Update ultimate_catch.py
    fixed positioning

--- ultimate_catch.py
+++ ultimate_catch.py
@@ -3,5 +3,6 @@ from fish_classes import Fish
 
 class UltimateCatch(Fish):
     def __init__(self, x, y):
-        super().__init__(x, y, speed=10, points=50)
+        super().__init__(x, y-100, speed=10, points=50)
         self.image = pygame.image.load('ultimate_catch.png')  # Replace 'moonfish.png' with the actual image file name
+
