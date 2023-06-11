6a4e75a16733c722372e12f4e9a5fc263ed1a7c8
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-11 02:06:51 -0700

    Add files via upload
    Whoa! That's some new files

---
+++ ultimate_catch.py
@@ -0,0 +1,7 @@
+import pygame
+from fish_classes import Fish
+
+class UltimateCatch(Fish):
+    def __init__(self, x, y):
+        super().__init__(x, y, speed=10, points=50)
+        self.image = pygame.image.load('ultimate_catch.png')  # Replace 'moonfish.png' with the actual image file name
