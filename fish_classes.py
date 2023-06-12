8921b754143ec26605fe280654bc7705a3b9c8f0
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-12 00:29:14 -0700

    Update fish_classes.py

--- fish_classes.py
+++ fish_classes.py
@@ -7,22 +7,15 @@ class Fish:
     def __init__(self, x, y, speed, points):
         self.x = x
         self.y = y
-        self.image_choice = self.image_list_from_folder("common_fish_game")
-        self.image = pygame.image.load(self.image_choice)  # Replace 'fish.png' with the actual image file name
+        self.folder_name = "common_fish_game"
+        self.image_choice = self.image_list_from_folder(self.folder_name)
+        # Replace 'self.image_choice' with the image file name and path
+        self.image = pygame.image.load(self.image_choice)
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
         self.speed = speed
         self.points = points
 
-    def update(self, screen_width, screen_height):
-        self.x += self.speed
-        if self.x > screen_width:
-            self.x = 0
-        self.rect.center = (self.x, self.y)
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-    
     def image_list_from_folder(self,folder_name):
         """Function creeates an absolute path for a folder
         and creates a list of the images found.
@@ -45,23 +38,32 @@ class Fish:
         image_choice = str_abs_path + "/" + rand_choice
         return image_choice
 
+    def update(self, screen_width, screen_height):
+        self.x += self.speed
+        if self.x > screen_width:
+            self.x = 0
+            rand_int = random.randint(-250, 170)
+            self.y = 383 + rand_int
+        self.rect.center = (self.x, self.y)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
 class Common(Fish):
     def __init__(self, x, y):
-        super().__init__(x, y-50, speed=2, points=5)
+        super().__init__(x, y, speed=2, points=5)
         self.found_fish = self.image_list_from_folder("common_fish_game")
         pygame.image.load(self.found_fish)  # Replace 'red_snapper.png' with the actual image file name
 
-
 class Uncommon(Fish):
     def __init__(self, x, y):
-        super().__init__(x, y-200, speed=4, points=10)
+        super().__init__(x, y, speed=6, points=10)
         self.found_fish = self.image_list_from_folder("uncommon_fish_game")
         self.image = pygame.image.load(self.found_fish)  # Replace 'cod.png' with the actual image file name
 
-
 class Rare(Fish):
     def __init__(self, x, y):
-        super().__init__(x, y+200, speed=6, points=15)
+        super().__init__(x, y, speed=8, points=15)
         self.found_fish = self.image_list_from_folder("rare_fish_game")
         self.image = pygame.image.load(self.found_fish)  # Replace 'moonfish.png' with the actual image file name
+
