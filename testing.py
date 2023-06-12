d20848d4889b4f9a181b318004a59932531acac2
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-11 19:15:52 -0700

    Update testing.py
    Testing absolute path for the background image

--- testing.py
+++ testing.py
@@ -1,1 +1,133 @@
+'''
+File: Final_main.py
+Authors: Sarena, Aspen, and Sam
+This file contains the main build for the fishing game. 
+'''
+
+import pygame
+
+from pathlib import Path
+from os import listdir
+
+from fish_classes import Common, Uncommon, Rare
+from ultimate_catch import UltimateCatch
+from fisher_cat_class import FisherCat
+from score import Scoreboard
+
+
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+pygame.init()
+
+#Window set up
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+run = True
+pygame.display.set_caption('Fish Game')
+clock = pygame.time.Clock()
+
+
+# Load the background image
+path = Path("misc_sprites_and_background")
+abs_path = Path(path).resolve()
+
+for images in listdir(abs_path):
+    if images == "background.png":
+        bg_img = images
+        print(abs_path)
+        print(bg_img)  # Replace 'background.png' with the actual image file name
+
+str_abs = str(abs_path)
+
+background_image = pygame.image.load(str_abs+"/"+bg_img)  # Replace 'background.png' with the actual image file name
+
+# Resize the background image to fit the window
+background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# music
+# pygame.mixer.init()
+# pygame.mixer.music.load('background_music.wav')
+# pygame.mixer.music.play(-1)
+
+# splash sound
+
+# def fish_caught_sound():
+#     pygame.mixer.Channel(0).play(pygame.mixer.Sound("splash_sound.wav"))
+
+# Create fish objects
+# common_fish = Common(WINDOW_WIDTH // 2 - 100, WINDOW_HEIGHT // 2 + 150)
+# uncommon_fish = Uncommon(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 100)
+# rare_fish = Rare(WINDOW_WIDTH // 2 + 100, WINDOW_HEIGHT // 2 + 75)
+# ultimate_catch = UltimateCatch(WINDOW_WIDTH // 2 +100, WINDOW_HEIGHT // 2 +75)
+
+#Create cat fisherman
+# cat = FisherCat(150, 100, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+# Create the scoreboard object
+scoreboard = Scoreboard()
+
+pygame.key.set_repeat(True)
+
+
+# while run:
+#     pygame.display.update()
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_d:
+#                 cat.move_right()
+#             elif event.key == pygame.K_a:
+#                 cat.move_left()
+#             elif event.key == pygame.K_e:
+#                 cat.ready_cast()
+#         if event.type == pygame.KEYUP:
+#             if event.key == pygame.K_e:
+#                 cat.cast()
+
+    # # Check for collision between the fishing bob and the fish
+    # if cat.bob_rect.colliderect(common_fish.rect):
+    #     fish_caught_sound()
+    #     scoreboard.increase_score(common_fish.points)
+    #     cat.reset_bob()
+    # if cat.bob_rect.colliderect(uncommon_fish.rect):
+    #     fish_caught_sound()
+    #     scoreboard.increase_score(uncommon_fish.points)
+    #     cat.reset_bob()
+    # if cat.bob_rect.colliderect(rare_fish.rect):
+    #     fish_caught_sound()
+    #     scoreboard.increase_score(rare_fish.points)
+    #     cat.reset_bob()
+    # if cat.bob_rect.colliderect(ultimate_catch.rect):
+    #     fish_caught_sound()
+    #     scoreboard.increase_score(ultimate_catch.points)
+    #     cat.reset_bob()
+
+    # Update fish positions
+    # common_fish.update(WINDOW_WIDTH, WINDOW_HEIGHT)
+    # uncommon_fish.update(WINDOW_WIDTH, WINDOW_HEIGHT)
+    # rare_fish.update(WINDOW_WIDTH, WINDOW_HEIGHT)
+    # ultimate_catch.update(WINDOW_WIDTH,WINDOW_HEIGHT)
+
+    # Draw the background image
+window.blit(background_image, (0, 0))
+
+    # Draw the fish
+    # common_fish.draw(window)
+    # uncommon_fish.draw(window)
+    # rare_fish.draw(window)
+    # ultimate_catch.draw(window)
+    # cat.draw(window)
+
+    # Draw the scoreboard
+    # scoreboard.draw(window)
+
+    # Update the display
+pygame.display.flip()
+
+    # Control the frame rate
+clock.tick(60)
+input()
+pygame.quit()
 
