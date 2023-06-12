1d07fd4723950a4fb1b5959c83ac57583b85d435
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-12 00:29:49 -0700

    Update ultimate_catch.py

--- ultimate_catch.py
+++ ultimate_catch.py
@@ -4,8 +4,6 @@ from fish_classes import Fish
 
 class UltimateCatch(Fish):
     def __init__(self, x, y):
-        x = random.randint(150, 300)
-        super().__init__(x, y-100, speed=10, points=50)
+        super().__init__(x, y, speed=10, points=50)
         self.found_fish = self.image_list_from_folder("ultimate_fish_game")
-        self.image = pygame.image.load(self.found_fish)  # Replace 'moonfish.png' with the actual image file name
-
+        self.image = pygame.image.load(self.found_fish)
