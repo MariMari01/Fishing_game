bc78b58044cf41726176daeadf181d76ffbfbee5
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-13 13:24:54 -0700

    Update ultimate_catch.py

--- ultimate_catch.py
+++ ultimate_catch.py
@@ -1,10 +1,11 @@
 import pygame
 import random
+import time
 from fish_classes import Fish
 
 class UltimateCatch(Fish):
     def __init__(self, x, y):
-        super().__init__(x, y, speed=10, points=50)
+        super().__init__(x, y, speed=20, points=500)
         self.found_fish = self.image_list_from_folder("ultimate_fish_game")
         self.image = pygame.image.load(self.found_fish)
     
@@ -16,4 +17,5 @@ class UltimateCatch(Fish):
             self.x = 0
             rand_int = random.randint(-180, 170)
             self.y = 383 + rand_int
+
         self.rect.center = (self.x, self.y)
