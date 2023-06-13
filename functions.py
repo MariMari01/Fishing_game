9ada9ed1dd42cb9977467857ec44d178d39a890e
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-12 21:59:55 -0700

    Update functions.py
    deleted model

--- functions.py
+++ functions.py
@@ -1,7 +1,29 @@
-from model import folder_search
+
 from pathlib import Path
 from os import listdir
 import pygame
+
+
+def folder_search(folder_name, file_name):
+    """Function creeates an absolute path for a folder
+    and searches the folder for a file.
+
+    Args:
+        folder_name (folder file): A folder that holds files for the game
+        file_name (.png or .wav): A file that will be used in the game,
+        either a .png or a .wav file
+
+    Returns:
+        str: The absolute path and file name are returned as strings.
+    """
+    path = Path(folder_name)
+    abs_path = Path(path).resolve()
+    for images in listdir(abs_path):
+        if images == file_name:
+            found_image = images
+    str_abs_path = str(abs_path)
+    file_abs = str_abs_path + "/" + found_image # Concatonates the file to the absolute path
+    return file_abs
 
 def background_music():
     """Allows the background music to play throughout the gameplay.
@@ -44,3 +66,4 @@ def cat_animation(window, x,y):
         window.blit(image, (x,y))
         pygame.display.update()
         value += 1
+
