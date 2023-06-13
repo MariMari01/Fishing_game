4fd9f760614154a642c9c7f02f1bc29dda136563
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2023-06-12 18:44:06 -0700

    Update ultimate_catch.py

--- ultimate_catch.py
+++ ultimate_catch.py
@@ -7,3 +7,13 @@ class UltimateCatch(Fish):
         super().__init__(x, y, speed=10, points=50)
         self.found_fish = self.image_list_from_folder("ultimate_fish_game")
         self.image = pygame.image.load(self.found_fish)
+    
+    def update(self, screen_width, screen_height):
+        self.x += self.speed
+        if self.x >= screen_width:
+            self.found_fish = self.image_list_from_folder("ultimate_fish_game")
+            self.image = pygame.image.load(self.found_fish)
+            self.x = 0
+            rand_int = random.randint(-180, 170)
+            self.y = 383 + rand_int
+        self.rect.center = (self.x, self.y)
