242801b529b9bd1a83c54f358b913842b6ab4c1c
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-14 00:30:34 -0700

    Update start_game.py

--- start_game.py
+++ start_game.py
@@ -1,7 +1,7 @@
 """Fishing Game by Sam, Sarena, and Aspen
 Module for the Start Screen Start Game button.
 """
-
+import time
 import pygame
 import functions as f
 from fish_classes import Common, Uncommon, Rare, UltimateCatch
@@ -30,7 +30,7 @@ def start_game():
     clock = pygame.time.Clock()
 
     # Change counter for testing
-    counter = 2
+    counter = 10
     font = pygame.font.SysFont(None, 36)
     text_color = (255, 255, 255)
     position = (640, 11)
@@ -75,13 +75,13 @@ def start_game():
                 counter -= 1
                 text = font.render("Time: " + str(counter), True, text_color)
             # Change scoreboard.score for testing
-            if not game_is_over and counter <= 0 and scoreboard.score < 2000:
+            if counter <= 0 and scoreboard.score < 20:
                 pygame.time.set_timer(timer_event, 0)
                 game_is_over = True
-
+                f.music_end()
+                f.game_over_sound()
                 game_over.draw(window)
-            
-            if counter <= 0 and scoreboard.score >= 2000:
+            if counter <= 0 and scoreboard.score >= 20:
                 pygame.time.set_timer(timer_event, 0)
                 game_is_over = True
                 f.game_won(window, WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -158,3 +158,4 @@ def start_game():
         # Control the frame rate
         clock.tick(60)
     pygame.quit()
+
