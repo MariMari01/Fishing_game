63971d3312075f309d2939ef832847001807d056
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-13 18:13:34 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -93,3 +93,46 @@ class GameOver:
         music_end()
         self.score_text = self.font.render("GAME OVER", True, self.text_color)
         screen.blit(self.score_text, self.position)
+
+class Scoreboard:
+    def __init__(self):
+        self.score = 0
+        self.font = pygame.font.SysFont(None, 36)
+        self.text_color = (255, 255, 255)
+        self.position = (10, 10)
+
+    def increase_score(self, points):
+        self.score += points
+
+    def draw(self, screen):
+        score_text = self.font.render("Score: " + str(self.score), True, self.text_color)
+        screen.blit(score_text, self.position)
+
+class CastBar:
+
+    def __init__(self) -> None:
+        self.fill_height = 0
+        self.bottom = 258
+        self.top = 102
+        self.color = (146, 240, 235)
+        self.fill_color = (11, 252, 3)
+        self.xpos = 42
+        self.bar_ypos = 215
+        self.minigame_fishpos = 42
+
+    def draw(self, window):
+        # Drawing Rectangle
+        pygame.draw.rect(window, self.color, pygame.Rect(700, 100, 30, 120))
+
+    def fill_up(self):
+        '''
+        Moves the mini game bar up
+        '''
+        if self.bar_ypos <= self.top:
+            pass
+        else:
+            self.fill_height += 0.25
+            self.bar_ypos -= 0.25
+
+    def fill(self, window):
+        self.rectangle = pygame.draw.rect(window, self.fill_color, pygame.Rect(702, self.bar_ypos, 26, self.fill_height))
