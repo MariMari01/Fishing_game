6e07ce048e293e60e9aee64c98358d52de2e2da0
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-13 18:24:08 -0700

    Update Final_main.py
    weird glitch when the game ends but i made the imports prettier

--- Final_main.py
+++ Final_main.py
@@ -6,7 +6,7 @@ This file contains the main build for the fishing game.
 
 
 import pygame
-from functions import folder_search, background_music, fish_caught_sound, music_end, GameOver, game_won, cat_animation, Scoreboard, CastBar
+import functions as f
 from fish_classes import Common, Uncommon, Rare, UltimateCatch
 from fisher_cat_class import FisherCat
 
@@ -24,7 +24,7 @@ pygame.display.set_caption('Fish Game')
 
 # Set up for the timer functon
 clock = pygame.time.Clock()
-counter = 20
+counter = 10
 font = pygame.font.SysFont(None, 36)
 text_color = (255, 255, 255)
 position = (640, 11)
@@ -34,26 +34,26 @@ pygame.time.set_timer(timer_event, 1000)
 
 
 # Load the background image
-bg_img = folder_search("misc_sprites_and_background", "background.png")
+bg_img = f.folder_search("misc_sprites_and_background", "background.png")
 background_image = pygame.image.load(bg_img)
 background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-castbar = CastBar()
+castbar = f.CastBar()
 # Create fish objects
 common_fish = Common(WINDOW_WIDTH // 2 - 100, WINDOW_HEIGHT // 2 - 80)
 uncommon_fish = Uncommon(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 70)
 rare_fish = Rare(WINDOW_WIDTH // 2 + 100, WINDOW_HEIGHT // 2 + 260)
 ultimate_catch = UltimateCatch(WINDOW_WIDTH // 2 +100, WINDOW_HEIGHT // 2 + 150)
-game_over = GameOver(WINDOW_WIDTH, WINDOW_HEIGHT)
+game_over = f.GameOver(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 #Create cat fisherman
 cat = FisherCat(150, 100, WINDOW_WIDTH, WINDOW_HEIGHT)
 
 # Create the scoreboard object
-scoreboard = Scoreboard()
+scoreboard = f.Scoreboard()
 
 pygame.key.set_repeat(True)
-background_music()
+f.background_music()
 
 catching = False
 fish_caught_points = None
@@ -70,12 +70,12 @@ while run:
             text = font.render("Time: " + str(counter), True, text_color)
             if counter <= 0 and scoreboard.score < 100:
                 pygame.time.set_timer(timer_event, 0)
-                music_end()
+                f.music_end()
                 game_is_over = True
                 game_over.draw(window)
             if counter <= 0 and scoreboard.score >= 100:
                 game_is_over = True
-                game_won(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+                f.game_won(window, WINDOW_WIDTH, WINDOW_HEIGHT)
 
         if counter > 0 and event.type  == pygame.KEYDOWN:
             if event.key == pygame.K_d:
@@ -90,33 +90,33 @@ while run:
             if event.key == pygame.K_e:
                 castbar.fill_height = 0
                 castbar.bar_ypos = 215
-                cat_animation(window, cat.rect.left, cat.rect.top)
+                f.cat_animation(window, cat.rect.left, cat.rect.top)
                 cat.cast()
     # Check for collision between the fishing bob and the fish
     if cat.bob_rect.colliderect(common_fish.rect):
         catch_points = True
-        fish_caught_sound()
+        f.fish_caught_sound()
         fish_caught_points = common_fish.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
 
     if cat.bob_rect.colliderect(uncommon_fish.rect):
         catching = True
-        fish_caught_sound()
+        f.fish_caught_sound()
         fish_caught_points = uncommon_fish.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
 
     if cat.bob_rect.colliderect(rare_fish.rect):
         catching = True
-        fish_caught_sound()
+        f.fish_caught_sound()
         fish_caught_points = rare_fish.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
 
     if cat.bob_rect.colliderect(ultimate_catch.rect):
         catching = True
-        fish_caught_sound()
+        f.fish_caught_sound()
         fish_caught_points = ultimate_catch.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
@@ -134,17 +134,17 @@ while run:
         window.blit(background_image, (0, 0))
 
     # Draw the fish
-        common_fish.draw(window)
-        uncommon_fish.draw(window)
-        rare_fish.draw(window)
-        ultimate_catch.draw(window)
+    common_fish.draw(window)
+    uncommon_fish.draw(window)
+    rare_fish.draw(window)
+    ultimate_catch.draw(window)
 
-        cat.draw(window)
-        castbar.draw(window)
-        castbar.fill(window)
+    cat.draw(window)
+    castbar.draw(window)
+    castbar.fill(window)
 
     # Draw the scoreboard
-        scoreboard.draw(window)
+    scoreboard.draw(window)
     window.blit(text, position)
     # Update the display
     pygame.display.flip()
