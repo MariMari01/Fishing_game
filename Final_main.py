08e1ff0f3080d787ae25b178211f0ee582aeaced
Aspen Frazee <116313532+evilfrogking@users.noreply.github.com>
2023-06-13 18:03:38 -0700

    Update Final_main.py
    Merged everything together

--- Final_main.py
+++ Final_main.py
@@ -3,16 +3,15 @@ File: Final_main.py
 Authors: Sarena, Aspen, and Sam
 This file contains the main build for the fishing game. 
 '''
-
+import time
 import pygame
-from functions import background_music, fish_caught_sound, cat_animation
+from functions import folder_search, background_music, fish_caught_sound, music_end, GameOver, game_won, cat_animation
 from fish_classes import Common, Uncommon, Rare
 from ultimate_catch import UltimateCatch
 from fisher_cat_class import FisherCat
 from score import Scoreboard
-#from mini_game import mini_game
-from model import folder_search
 from cast_bar import CastBar
+
 pygame.init()
 pygame.mixer.init()
 
@@ -23,15 +22,22 @@ WINDOW_HEIGHT = 600
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 run = True
 pygame.display.set_caption('Fish Game')
+
+# Set up for the timer functon
 clock = pygame.time.Clock()
+counter = 20
+font = pygame.font.SysFont(None, 36)
+text_color = (255, 255, 255)
+position = (640, 11)
+text = font.render("Time: " + str(counter), True, text_color)
+timer_event = pygame.USEREVENT+1
+pygame.time.set_timer(timer_event, 1000)
 
 
 # Load the background image
 bg_img = folder_search("misc_sprites_and_background", "background.png")
 background_image = pygame.image.load(bg_img)
-# Resize the background image to fit the window
 background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
-
 
 castbar = CastBar()
 # Create fish objects
@@ -39,9 +45,7 @@ common_fish = Common(WINDOW_WIDTH // 2 - 100, WINDOW_HEIGHT // 2 - 80)
 uncommon_fish = Uncommon(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 70)
 rare_fish = Rare(WINDOW_WIDTH // 2 + 100, WINDOW_HEIGHT // 2 + 260)
 ultimate_catch = UltimateCatch(WINDOW_WIDTH // 2 +100, WINDOW_HEIGHT // 2 + 150)
-#minigame = mini_game(5)
-
-
+game_over = GameOver(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 #Create cat fisherman
 cat = FisherCat(150, 100, WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -52,13 +56,28 @@ scoreboard = Scoreboard()
 pygame.key.set_repeat(True)
 background_music()
 
+catching = False
+fish_caught_points = None
+game_is_over = False
+catch_points = 0
 while run:
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYDOWN:
+        elif event.type == timer_event:
+            counter -= 1
+            text = font.render("Time: " + str(counter), True, text_color)
+            if counter <= 0 and scoreboard.score < 100:
+                pygame.time.set_timer(timer_event, 0)
+                game_is_over = True
+                game_over.draw(window)
+            if counter <= 0 and scoreboard.score >= 100:
+                game_is_over = True
+                game_won(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+        
+        if not catching and counter > 0 and event.type  == pygame.KEYDOWN:
             if event.key == pygame.K_d:
                 cat.move_right()
             elif event.key == pygame.K_a:
@@ -75,37 +94,33 @@ while run:
                 cat.cast()
     # Check for collision between the fishing bob and the fish
     if cat.bob_rect.colliderect(common_fish.rect):
-
+        catch_points = True
         fish_caught_sound()
-
         fish_caught_points = common_fish.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
-        fish_caught_sound()
+
     if cat.bob_rect.colliderect(uncommon_fish.rect):
-
+        catching = True
         fish_caught_sound()
-
         fish_caught_points = uncommon_fish.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
-        fish_caught_sound()
+
     if cat.bob_rect.colliderect(rare_fish.rect):
-
+        catching = True
         fish_caught_sound()
-
         fish_caught_points = rare_fish.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
-        fish_caught_sound()
+
     if cat.bob_rect.colliderect(ultimate_catch.rect):
-
+        catching = True
         fish_caught_sound()
-
         fish_caught_points = ultimate_catch.points
         cat.reset_bob()
         scoreboard.increase_score(fish_caught_points)
-        fish_caught_sound()
+
 
 
     # Update fish positions
@@ -115,27 +130,26 @@ while run:
     ultimate_catch.update(WINDOW_WIDTH,WINDOW_HEIGHT)
 
     # Draw the background image
-    window.blit(background_image, (0, 0))
+    if not game_is_over:
+        window.blit(background_image, (0, 0))
 
     # Draw the fish
-    # -----------------------------------------------------------
-    # Uncomment ultimate_catch to see how it looks on screen!
-    # -----------------------------------------------------------
     common_fish.draw(window)
-    
-
     uncommon_fish.draw(window)
     rare_fish.draw(window)
-    #ultimate_catch.draw(window)
+    ultimate_catch.draw(window)
+
     cat.draw(window)
     castbar.draw(window)
     castbar.fill(window)
 
     # Draw the scoreboard
     scoreboard.draw(window)
+    window.blit(text, position)
     # Update the display
     pygame.display.flip()
 
     # Control the frame rate
     clock.tick(60)
 pygame.quit()
+
