6bb0b433480347ad0172722fd8a444d7d94c5921
Sarena Christoffersen <114170711+SarenaKC@users.noreply.github.com>
2023-06-13 19:01:10 -0700

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,4 +1,3 @@
-
 import pygame
 from functions import background_music, fish_caught_sound, cat_animation
 from fish_classes import Common, Uncommon, Rare
@@ -216,14 +215,125 @@ def start_game():
 
 def show_story():
     # Code to display the background story goes here
-    pass
+    # Game code goes here
+    pygame.init()
+    pygame.mixer.init()
+
+    #Window set up
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    run = True
+    pygame.display.set_caption('Fish Game')
+    clock = pygame.time.Clock()
+
+
+    # Load the background image
+    bg_img = folder_search("misc_sprites_and_background", "background.png")
+    background_image = pygame.image.load(bg_img)
+    # Resize the background image to fit the window
+    background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
 
 def show_encyclopedia():
     # Code to show the fish encyclopedia goes here
+    pygame.init()
+    pygame.mixer.init()
+
+    # Window setup
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    run = True
+    pygame.display.set_caption('Fish Game - Fish Encyclpedia')
+    clock = pygame.time.Clock()
+
+    # Load the background image
+    bg_img = folder_search("misc_sprites_and_background", "background.png")
+    background_image = pygame.image.load(bg_img)
+    background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # Button setup
+    button_width = 250
+    button_height = 50
+    button_margin = 20
+    button_color = (0, 0, 50) # Dark Blue
+
+    buttons = [
+        Button("Common Fish", WINDOW_WIDTH // 2 - button_width // 2, WINDOW_HEIGHT // 2 - 2.5 * button_height - 2 * button_margin, button_width, button_height, button_color),
+        Button("Uncommon Fish", WINDOW_WIDTH // 2 - button_width // 2, WINDOW_HEIGHT // 2 - 1.5 * button_height - button_margin, button_width, button_height, button_color),
+        Button("Rare Fish", WINDOW_WIDTH // 2 - button_width // 2, WINDOW_HEIGHT // 2 - 0.5 * button_height, button_width, button_height, button_color),
+        Button("Ultimate Fish", WINDOW_WIDTH // 2 - button_width // 2, WINDOW_HEIGHT // 2 + 0.5 * button_height + button_margin, button_width, button_height, button_color),
+        Button("Back", WINDOW_WIDTH // 2 - button_width // 2, WINDOW_HEIGHT // 2 + 1.5 * button_height + 2 * button_margin, button_width, button_height, button_color)
+    ]
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                for button in buttons:
+                    if button.is_mouse_over():
+                        if button.text == "Common Fish":
+                            common_fish()
+                        elif button.text == "Uncommon Fish":
+                            uncommon_fish()
+                        elif button.text == "Rare Fish":
+                            rare_fish()
+                        elif button.text == "Ultimate Fish":
+                            ultimate_fish()
+                        elif button.text == "Back":
+                            main_menu()
+                            return
+
+        window.blit(background_image, (0, 0))
+
+        for button in buttons:
+            if button.is_mouse_over():
+                button.draw(window)
+            else:
+                button.draw(window)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+def common_fish():
     pass
+
+def uncommon_fish():
+    pass
+
+def rare_fish():
+    pass
+
+def ultimate_fish():
+    pass
+
+
 
 def show_controls():
     # Code to show the controls goes here
-    pass
+    pygame.init()
+    pygame.mixer.init()
+
+    # Window setup
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    run = True
+    pygame.display.set_caption('Fish Game - Fish Encyclpedia')
+    clock = pygame.time.Clock()
+
+    # Load the background image
+    bg_img = folder_search("misc_sprites_and_background", "background.png")
+    background_image = pygame.image.load(bg_img)
+    background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
 
 main_menu()
+
